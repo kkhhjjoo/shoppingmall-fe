4ddff97970da4a623dd0c03211fbcc267ff20ba7
kkhhjjoo <khjoo1203@gmail.com>
2026-02-27 12:39:37 +0900

    구글 로그인

--- src/page/LoginPage/LoginPage.js
+++ src/page/LoginPage/LoginPage.js
@@ -1,20 +1,19 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Container, Form, Button, Alert } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { GoogleLogin } from "@react-oauth/google";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import "./style/login.style.css";
-import { loginWithEmail, loginWithGoogle } from "../../features/user/userSlice";
-import { clearErrors } from "../../features/user/userSlice";
-const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+import React, { useState, useEffect, useRef } from 'react';
+import { Container, Form, Button, Alert } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { GoogleLogin } from '@react-oauth/google';
+
+import './style/login.style.css';
+import { loginWithEmail, loginWithGoogle } from '../../features/user/userSlice';
+import { clearErrors } from '../../features/user/userSlice';
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { user, loginError } = useSelector((state) => state.user);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const loginPromiseRef = useRef(null);
 
   useEffect(() => {
@@ -30,7 +29,7 @@ const Login = () => {
 
   useEffect(() => {
     if (user) {
-      navigate("/");
+      navigate('/');
     }
   }, [user]);
 
@@ -41,6 +40,7 @@ const Login = () => {
 
   const handleGoogleLogin = async (googleData) => {
     //구글 로그인 하기
+    console.log('haha', googleData);
   };
   return (
     <>
@@ -53,43 +53,40 @@ const Login = () => {
         <Form className="login-form" onSubmit={handleLoginWithEmail}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              required
-              onChange={(event) => setEmail(event.target.value)}
-            />
+            <Form.Control type="email" placeholder="Enter email" required onChange={(event) => setEmail(event.target.value)} />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              required
-              onChange={(event) => setPassword(event.target.value)}
-            />
+            <Form.Control type="password" placeholder="Password" required onChange={(event) => setPassword(event.target.value)} />
           </Form.Group>
           <div className="display-space-between login-button-area">
             <Button variant="danger" type="submit">
               Login
             </Button>
             <div>
-              아직 계정이 없으세요?<Link to="/register">회원가입 하기</Link>{" "}
+              아직 계정이 없으세요?<Link to="/register">회원가입 하기</Link>{' '}
             </div>
           </div>
 
           <div className="text-align-center mt-2">
             <p>-외부 계정으로 로그인하기-</p>
             <div className="display-center">
-              <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-                <GoogleLogin
-                  onSuccess={handleGoogleLogin}
-                  onError={() => {
-                    console.log("Login Failed");
-                  }}
-                />
-              </GoogleOAuthProvider>
+              {/* 
+              1. 구글 로그인 가져오기
+              2. Oauth로그인을 위해 google api사이트에 가입하고 클라이언트키, 시크릿키 받아오기
+              3. 로그인
+              4. 백엔드에서 로그인하기
+              4-1. 이미 로그인을 한적이 있는 유저 => 로그인시키고 토큰값 주면 됨
+              4-2. 처음 로그인 시도를 한 유저다 -> 유저정보 먼저 새로 생성 => 토큰값
+              */}
+
+              <GoogleLogin
+                onSuccess={handleGoogleLogin}
+                onError={() => {
+                  console.log('Login Failed');
+                }}
+              />
             </div>
           </div>
         </Form>
